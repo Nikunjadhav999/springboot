151d8f93fd5a0bbd775f89d8504a816d88ec174f
Nikunjadhav999 <nikunjjadhav43@gmail.com>
2025-02-11 14:32:21 +0530

    commit to dev day9

---
+++ Task1/src/main/java/com/example/demo/Task1Application.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task1Application {
+
+	public static void main(String[] args) 
+	{
+		        System.out.println("Running SpringApplication");
+		        SpringApplication.run(SpringBootApplication.class, args);
+		        System.out.println("Done SpringApplication");
+		    }
+	}
+
+
+
