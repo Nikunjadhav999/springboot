ac4d063aab06835b6dd82188686fbb305777dd78
Nikunjadhav999 <nikunjjadhav43@gmail.com>
2025-02-11 17:46:45 +0530

    commit to dev day9

---
+++ sba/src/main/java/com/example/demo/SbaApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbaApplication.class, args);
+		System.out.println("SBA test");
+		System.out.println("test2");
+		
+		
+	}
+
+}
